dc42f8e9d9172da713fc58b4d4a3e1d1e2357b2d
LegendKaushal <74301612+LegendKaushal@users.noreply.github.com>
2021-04-12 16:49:03 +0530

    Add files via upload

---
+++ particles.js
@@ -0,0 +1,32 @@
+class Particle {
+    constructor(x) {
+        var options = {
+            restitution: 1,
+            friction: 0,
+              isStatic:false
+        }
+        this.r = 10;
+        this.body = Bodies.circle(x,10, this.r, options);
+        this.color = color(random(0,255),random(0,255),random(0,255))
+
+        
+        World.add(world, this.body);
+    }
+    display() {
+
+        var pos = this.body.position;
+        var angle = this.body.angle;
+
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        imageMode(CENTER);
+        noStroke();
+        var r = random(1,255) 
+        fill(this.color);
+        ellipseMode(RADIUS);
+        ellipse(0,0,this.r,this.r);
+        pop();
+    }
+
+};
